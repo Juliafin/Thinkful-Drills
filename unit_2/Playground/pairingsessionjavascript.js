a9fb98b1568b62c54f3af2f6b188f49e47055c88
Juliafin <juliafinarovsky@gmail.com>
2017-01-27 23:03:32 -0500

    updated naming

---
+++ unit_2/Playground/pairingsessionjavascript.js
@@ -0,0 +1,48 @@
+var pageViewCounts = {
+  homePage: 399,
+  aboutPage: 400,
+  termsOfService: 22
+};
+
+console.log(Object.keys(pageViewCounts));
+
+function adding( element, ) {
+  console.log(_)
+}
+
+var instruments = {}
+Object.keys(pageViewCounts).forEach(function(key){
+  console.log(
+    'the `' + key + '` page has ' + pageViewCounts[key] +
+    ' views.'
+  );
+})
+
+var someAry = [1, 2, 3];
+var newAry = [];
+// null, 0, ""
+forEach(function(item, index, someAry) {
+  if(index === 0) {
+    newArray.push(undefined);
+    newArray.push(item);
+  } else {
+    newArray.push(item);
+  }
+});
+
+for (var i=0; i<ary.length; i++) {
+  var item = ary[i];
+  var index = i;
+  var array = ary;
+
+  function yourFunction(item, index, array) {
+    // whatever you decided
+  }
+
+  yourFunction(item, index, array);
+}
+// ary = [2, 3, 4]
+
+[1, 2, 3].map(function(item, index, ary) {
+  return item + 1;
+});
